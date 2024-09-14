a7f9d3708c9d80058e0ce66905a9c9ee02e092ef
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-09-14 16:30:37 +0530

    env added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,9 @@
 require('dotenv').config(); 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('krishi', 'root', '#abhi0052ak', {
-    host: '127.0.0.1', 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',          
 });
 
