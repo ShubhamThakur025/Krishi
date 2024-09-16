ee0469188d6cefdf0f91789ebefff2b5120867f1
ShubhamThakur025 <thakur0805@gmail.com>
2024-09-16 23:29:38 +0530

    Added signup

--- Frontend/src/pages/InitialSetup/UserInfo.jsx
+++ Frontend/src/pages/InitialSetup/UserInfo.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Box, Typography, Button, Card } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 function UserInfo() {
+    const userData = useSelector(state => state.user.choices)
+    const { role, Name, Phone, Crops, City, State, Category } = userData
+    const navigate = useNavigate()
+
+    const registerUser = (e) => {
+        e.preventDefault()
+        const commonData = { name: Name, mobile: Phone, generalCrops: Crops, location: `${City}, ${State}` }
+        const uri = role === 'Farmer' ? 'http://localhost:8080/farmer/register' : 'http://localhost:8080/buyer/register';
+        const requestData = role == 'Farmer' ? commonData : { ...commonData, category: Category }
+        axios.post(uri, requestData)
+            .then(response => {
+                console.log(response)
+                toast.success('Registration successfull!')
+                navigate('/dashboard')
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        console.log(userData)
+    }, [userData])
+
     const choices = useSelector(state => state.user.choices)
     return (
         <Box component="section" sx={{ p: 2, display: 'flex', flexDirection: 'column', textAlign: 'center', justifyContent: 'center', gap: 2, height: '100vh' }}>
@@ -29,9 +53,9 @@ function UserInfo() {
                 <Typography variant="body2" sx={{ color: 'text.secondary', width: 300, my: 2 }}>
                     We shall be proceeding towards setting up an account for you. Thus, you agree to the terms and conditions that are listed below
                 </Typography>
-                <Link to={'/dashboard'}><Button variant="contained" sx={{ width: 200, mx: 'auto', mt: 3 }}>Submit <NavigateNextIcon /></Button></Link>
+                <Button variant="contained" onClick={registerUser} sx={{ width: 200, mx: 'auto', mt: 3 }}>Submit <NavigateNextIcon /></Button>
             </Card>
-        </Box>
+        </Box >
     )
 }
 
